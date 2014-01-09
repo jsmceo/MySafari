ecd91a07f5118a62114dc9a8bf185d36f9f09596
fragdabble <marcgalang@gmail.com>
2014-01-09 13:38:25 -0600

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Marcial Galang on 1/9/14.
+//  Copyright (c) 2014 Marc Galang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
